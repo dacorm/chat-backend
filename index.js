80c5fecb86c1bc38cffb17ae06694a54eb3bd755
Utkin Denis <stolen.youth@yandex.ru>
2022-08-12 19:24:38 +0700

    fix: little fixes

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://snappy-chatapp-dacorm.herokuapp.com",
         credentials: true,
     }
 })
